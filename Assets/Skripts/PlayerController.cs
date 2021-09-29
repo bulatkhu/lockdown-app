0a7beb9725f2a38049da00fe41be7a4e330f800e
Grishko Antonina <antonina@Grishko-MacBook-Pro.local>
2021-09-29 22:41:09 +0300

    update 1

---
+++ Assets/Skripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+if (moveInput == 0)
+        {
+            animator.SetBool("isRunning", false);
+        }
+else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            spriteRenderer.flipX = true;
+        }
+      else
+        {
+            animator.SetBool("isRunning", true);
+            spriteRenderer.flipX = false;
+        }
+
+    }
+}
