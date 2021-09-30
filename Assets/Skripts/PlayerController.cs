4c3298a9df80554a97c60f2db7bf39254c6128ac
Your Name <you@example.com>
2021-09-30 11:35:13 +0300

    fff

--- Assets/Skripts/PlayerController.cs
+++ Assets/Skripts/PlayerController.cs
@@ -2,48 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
-
     public float speed;
     private Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     private float moveInput;
+    public int maxHP = 100;
+    public int currentHP;
+    public HealthBar hp;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
+        currentHP = maxHP;
+        hp.MaxHp(maxHP);
+        
     }
 
     void Update()
     {
 
-    }
-
-    void FixedUpdate()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-if (moveInput == 0)
+        if (moveInput == 0)
         {
             animator.SetBool("isRunning", false);
+
         }
-else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             animator.SetBool("isRunning", true);
             spriteRenderer.flipX = true;
+
         }
-      else
+        else
         {
             animator.SetBool("isRunning", true);
             spriteRenderer.flipX = false;
+
+        }
+
+        //for HP example
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
         }
 
     }
+
+    void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        hp.CurrentHp(currentHP);
+    }
 }
+
