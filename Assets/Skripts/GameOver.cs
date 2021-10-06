294044caec807f89c40ebe60f657400446d47715
Кудайназаров Булат <hudainazarov.b@yandex.ru>
2021-10-06 12:45:41 +0300

    new-game-over

---
+++ Assets/Skripts/GameOver.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        // SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
